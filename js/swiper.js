a738c300fb91d122b9a9758a408060041d8f6059
yrangadabatiec <licaza800@gmail.com>
2024-10-22 01:06:57 +0800

    2

--- js/swiper.js
+++ js/swiper.js
@@ -1,10 +1,14 @@
+var width = window.screen.width
+console.log(width);
 document.addEventListener('DOMContentLoaded', function () {
+  if (width >= 1600) {
+    console.log('1600');
     var swiper = new Swiper('.swiper-container', {
       slidesPerView: 2,
       spaceBetween: 32,
       autoplay: {
-          delay: 7500,
-          disableOnInteraction: false,
+        delay: 7500,
+        disableOnInteraction: false,
       },
       navigation: {
         nextEl: '.swiper-button-next',
@@ -15,4 +19,24 @@ document.addEventListener('DOMContentLoaded', function () {
         clickable: true,
       },
     });
-  });
+  }
+  else if (width < 1600 || width >= 800) {
+    console.log('min800');
+    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      spaceBetween: 16,
+      autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+  }
+});
